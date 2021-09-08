7ca2b25e547b9645d161a60a4f4a9e4bcbd40078
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-09-08 09:44:49 +0200

    usings spaces (and not tabs :'()

--- src/tree.ts
+++ src/tree.ts
@@ -12,257 +12,257 @@ class Node {
   #lastChild:Node|null = null
   props:Record<string,any>|null = null
 
-	constructor(props:Record<string,any>|null = null) {
+  constructor(props:Record<string,any>|null = null) {
 
-		Object.defineProperty(this, id, { enumerable: true, value: nodeUID++ })
+    Object.defineProperty(this, id, { enumerable: true, value: nodeUID++ })
 
-		this.props = props
-	}
+    this.props = props
+  }
 
-	get name() { return this.props?.name ?? `${this.constructor.name}#${this[id]}` }
+  get name() { return this.props?.name ?? `${this.constructor.name}#${this[id]}` }
 
-	append<T extends Node>(...children:T[]) {
+  append<T extends Node>(...children:T[]) {
 
-		for (const child of children) {
+    for (const child of children) {
 
-			if (child[id] === this[id]) {
-				throw new Error(`Could not append node to himself!`)
-			}
-
-			if (child.contains(this)) {
-				throw new Error(`The new child contains the parent!`)
-			}
-
-			if (child.#parent) {
-				child.#parent.remove(child)
+      if (child[id] === this[id]) {
+        throw new Error(`Could not append node to himself!`)
       }
 
-			child.#parent = this
-			child.#root = this.#root
-			for (const subchild of child.allChildren()) {
-				subchild.#root = this.#root
-			}
-
-			if (this.#lastChild) {
-
-				this.#lastChild.#next = child
-				child.#previous = this.#lastChild
-				this.#lastChild = child
-
-			} else {
-
-				this.#firstChild =
-				this.#lastChild = child
-			}
-		}
-
-		return this
-	}
-
-	remove<T extends Node>(...children:T[]) {
-
-		for (const child of children) {
-
-			if (this.#lastChild?.[id] === child[id]) {
-				this.#lastChild = child.#previous
+      if (child.contains(this)) {
+        throw new Error(`The new child contains the parent!`)
       }
 
-			if (this.#firstChild?.[id] === child[id]) {
-				this.#firstChild = child.#next
+      if (child.#parent) {
+        child.#parent.remove(child)
       }
 
-			const previous = child.#previous
-			const next = child.#next
-
-			if (previous) {
-				previous.#next = next
+      child.#parent = this
+      child.#root = this.#root
+      for (const subchild of child.allChildren()) {
+        subchild.#root = this.#root
       }
 
-			if (next) {
-				next.#previous = previous
+      if (this.#lastChild) {
+
+        this.#lastChild.#next = child
+        child.#previous = this.#lastChild
+        this.#lastChild = child
+
+      } else {
+
+        this.#firstChild =
+        this.#lastChild = child
+      }
+    }
+
+    return this
+  }
+
+  remove<T extends Node>(...children:T[]) {
+
+    for (const child of children) {
+
+      if (this.#lastChild?.[id] === child[id]) {
+        this.#lastChild = child.#previous
       }
 
-			child.#parent = null
-			child.#root = child
-			child.#previous = null
-			child.#next = null
-
-			for (const subchild of child.allChildren()) {
-				subchild.#root = child
-			}
-		}
-
-		return this
-	}
-
-	removeAll() {
-
-		let child = this.#firstChild
-
-		while (child) {
-
-			const next = child.#next
-
-			child.#parent = null
-			child.#root = child
-			child.#previous = null
-			child.#next = null
-
-			for (const subchild of child.allChildren()) {
-				subchild.#root = child
-			}
-
-			child = next
-		}
-
-		this.#firstChild = null
-		this.#lastChild = null
-
-		return this
-	}
-
-	appendTo(parent:Node) {
-
-		parent.append(this)
-
-		return this
-	}
-
-	detach() {
-		this.#parent?.remove(this)
-		return this
-	}
-
-	walk<T extends Node>(callback:(node:T) => void) {
-
-		callback(this as unknown as T)
-
-		let child = this.#firstChild
-
-		while(child) {
-			child.walk(callback)
-			child = child.#next
-		}
-
-		return this
-	}
-
-	// hierarchy test:
-
-	get depth() {
-		let count = 0
-		let node = this.#parent
-		while(node) {
-			count++
-			node = node.#parent
-		}
-		return count
-	}
-
-	get isRoot() {
-		return this.#root === this
-	}
-
-	get isDetached() {
-		return this.#root === this && !this.#parent && !this.#next && !this.#previous
-	}
-
-	get isTip() {
-		return this.#firstChild === null
-	}
-
-	// https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
-	contains(node:Node|null) {
-
-		while (node) {
-			if (node.#parent === this) {
-				return true
+      if (this.#firstChild?.[id] === child[id]) {
+        this.#firstChild = child.#next
       }
-			node = node.#parent
-		}
 
-		return false
-	}
+      const previous = child.#previous
+      const next = child.#next
 
-	isContainedBy(node:Node) {
-		return node.contains(this)
-	}
+      if (previous) {
+        previous.#next = next
+      }
+
+      if (next) {
+        next.#previous = previous
+      }
+
+      child.#parent = null
+      child.#root = child
+      child.#previous = null
+      child.#next = null
+
+      for (const subchild of child.allChildren()) {
+        subchild.#root = child
+      }
+    }
+
+    return this
+  }
+
+  removeAll() {
+
+    let child = this.#firstChild
+
+    while (child) {
+
+      const next = child.#next
+
+      child.#parent = null
+      child.#root = child
+      child.#previous = null
+      child.#next = null
+
+      for (const subchild of child.allChildren()) {
+        subchild.#root = child
+      }
+
+      child = next
+    }
+
+    this.#firstChild = null
+    this.#lastChild = null
+
+    return this
+  }
+
+  appendTo(parent:Node) {
+
+    parent.append(this)
+
+    return this
+  }
+
+  detach() {
+    this.#parent?.remove(this)
+    return this
+  }
+
+  walk<T extends Node>(callback:(node:T) => void) {
+
+    callback(this as unknown as T)
+
+    let child = this.#firstChild
+
+    while(child) {
+      child.walk(callback)
+      child = child.#next
+    }
+
+    return this
+  }
+
+  // hierarchy test:
+
+  get depth() {
+    let count = 0
+    let node = this.#parent
+    while(node) {
+      count++
+      node = node.#parent
+    }
+    return count
+  }
+
+  get isRoot() {
+    return this.#root === this
+  }
+
+  get isDetached() {
+    return this.#root === this && !this.#parent && !this.#next && !this.#previous
+  }
+
+  get isTip() {
+    return this.#firstChild === null
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
+  contains(node:Node|null) {
+
+    while (node) {
+      if (node.#parent === this) {
+        return true
+      }
+      node = node.#parent
+    }
+
+    return false
+  }
+
+  isContainedBy(node:Node) {
+    return node.contains(this)
+  }
 
 
 
-	// iterators:
+  // iterators:
 
-	*children<T extends Node>():Generator<T> {
+  *children<T extends Node>():Generator<T> {
 
-		let child = this.#firstChild
+    let child = this.#firstChild
 
-		while(child) {
-			yield child as T
-			child = child.#next
-		}
-	}
+    while(child) {
+      yield child as T
+      child = child.#next
+    }
+  }
 
-	// *allChildren<T extends Node>():Generator<T> {
+  // *allChildren<T extends Node>():Generator<T> {
 
-	// 	let child = this.#firstChild
+  // 	let child = this.#firstChild
 
-	// 	while(child) {
-	// 		yield child as T
-	// 		yield* child.allChildren()
-	// 		child = child.#next
-	// 	}
-	// }
+  // 	while(child) {
+  // 		yield child as T
+  // 		yield* child.allChildren()
+  // 		child = child.#next
+  // 	}
+  // }
 
-	*allChildren<T extends Node>({ 
-	
-		includeSelf = false, 
-		filter = undefined,
-		progression = 'vertical',
-	
-	}:Partial<{
+  *allChildren<T extends Node>({ 
+  
+    includeSelf = false, 
+    filter = undefined,
+    progression = 'vertical',
+  
+  }:Partial<{
 
-		includeSelf:boolean
-		filter:(node:T) => boolean
-		progression:'horizontal'|'vertical'
+    includeSelf:boolean
+    filter:(node:T) => boolean
+    progression:'horizontal'|'vertical'
 
-	}> = {}):Generator<T> {
+  }> = {}):Generator<T> {
 
-		if ((progression === 'horizontal' || progression === 'vertical') === false) {
-			throw new Error(`oops "progression" value should be "horizontal" or "vertical" (received "${progression}")`)
-		}
+    if ((progression === 'horizontal' || progression === 'vertical') === false) {
+      throw new Error(`oops "progression" value should be "horizontal" or "vertical" (received "${progression}")`)
+    }
 
-		const queue = (includeSelf ? [this] : [...this.children()]) as T[]
+    const queue = (includeSelf ? [this] : [...this.children()]) as T[]
 
-		while (queue.length > 0) {
-			const node = queue.shift()!
-			if (!filter || filter(node)) {
-				if (progression === 'horizontal') {
-					queue.push(...node.children<T>())
-				} else {
-					queue.unshift(...node.children<T>())
-				}
-				yield node
-			}
-		}
-	}
-	
-	/**
-	 * Returns all the parents in ASCENDING order.
-	 */
-	*allParents<T extends Node>():Generator<T> {
+    while (queue.length > 0) {
+      const node = queue.shift()!
+      if (!filter || filter(node)) {
+        if (progression === 'horizontal') {
+          queue.push(...node.children<T>())
+        } else {
+          queue.unshift(...node.children<T>())
+        }
+        yield node
+      }
+    }
+  }
+  
+  /**
+   * Returns all the parents in ASCENDING order.
+   */
+  *allParents<T extends Node>():Generator<T> {
 
-		let node = this.#parent
+    let node = this.#parent
 
-		while(node) {
-			yield node as T
-			node = node.#parent
-		}
-	}
+    while(node) {
+      yield node as T
+      node = node.#parent
+    }
+  }
 
-	allParentsInOrder<T extends Node>(descending = true) {
-		const parents = [...this.allParents<T>()]
-		return descending ? parents.reverse() : parents
-	}
+  allParentsInOrder<T extends Node>(descending = true) {
+    const parents = [...this.allParents<T>()]
+    return descending ? parents.reverse() : parents
+  }
 
   // getter
   get root() {
@@ -271,35 +271,35 @@ class Node {
   get parent() {
     return this.#parent
   }
-	get previous() {
-		return this.#previous
-	}
-	get next() {
-		return this.#next
-	}
+  get previous() {
+    return this.#previous
+  }
+  get next() {
+    return this.#next
+  }
 
 
 
-	// toGraphString:
+  // toGraphString:
 
-	toGraphStringLine() {
+  toGraphStringLine() {
 
-		const intro = []
+    const intro = []
 
-		for (const parent of this.allParents()) {
-			intro.unshift(!!parent.#next ? '│ ' : '  ')
+    for (const parent of this.allParents()) {
+      intro.unshift(!!parent.#next ? '│ ' : '  ')
     }
 
-		return (
+    return (
       intro.join('') +
         (this.#parent === null ? 'x' : (this.#next ? '├' : '└')) +
         '─' + (this.#firstChild ? '┬' : '─') + '─ ' + this.name
     )
-	}
+  }
 
-	toGraphString() {
-		return [this, ...this.allChildren()].map(node => node.toGraphStringLine()).join('\n')
-	}
+  toGraphString() {
+    return [this, ...this.allChildren()].map(node => node.toGraphStringLine()).join('\n')
+  }
 }
 
 export { Node }
